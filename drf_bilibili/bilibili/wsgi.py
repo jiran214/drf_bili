46d43416c5fd8712eb8ea29da6de9d56d6913c3e
yuchen59384 <7807098+yuchen59384@user.noreply.gitee.com>
2022-12-24 18:35:38 +0800

    部署准备

--- drf_bilibili/bilibili/wsgi.py
+++ drf_bilibili/bilibili/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilibili.settings')
+profile = os.environ.get("PROJECT_PROFILE", 'prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilibili.settings.{}'.format(profile))
 
 application = get_wsgi_application()
